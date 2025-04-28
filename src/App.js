d7ad6e4d3a2b9a3505a1e156ee1823b14a63f8d4
AbhishekGupta <=>
2025-04-29 00:41:54 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Employees} from "./data.js";
+import { useState,useEffect } from "react";
 function App() {
+
+  const [data , setData] = useState([]);
+  const [update , setUpdate] = useState(true);
+
+  const [name , setName] = useState('');
+  const [age , setAge] = useState('');
+
+  useEffect(()=>{
+    setData(Employees);
+   },[]);
+   
+  const toDelete = (id) =>{
+    const value = data.filter((i) => i.id !== id );
+    setData(value);
+  }
+  
+  const toEdit = (id) =>{
+    const val = data.filter((item)=> item.id === id);
+     if(val != undefined){
+       setName(val[0].name);
+       setAge(val[0].age);
+     }
+    setUpdate(false);
+
+  }
+
+  const toClear = () =>{
+
+    setName("");
+    setAge("");
+    setUpdate(true);
+  }
+    
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-center">
+      <div className="mt-5">
+
+        <label >Enter Name</label> 
+        <input placeholder="Enetr Your Name" onChange={(e) => setName(e.target.value)}  value={name}  />&nbsp;&nbsp;&nbsp;
+        <label >Enter Age</label>
+        <input placeholder="Enetr Your Name"  onChange={(e) => setName(e.target.value)}  value={age}/>&nbsp;&nbsp;
+        {
+          !update ? 
+           <button className="btn  btn-primary" >Update</button>
+          :
+          <button  className="btn btn-primary ">Save</button>
+
+        }&nbsp;&nbsp;&nbsp;
+        <button className="btn  btn-primary" onClick={()=> toClear()}>Clear</button>
+      </div>
+      <table  className="table table-success table-striped table-bordered mt-5 ">
+        <tr className="table-active" style={{backgroundColor:"gray"}}>
+           <th>ID</th>
+           <th>Name</th>
+           <th>AGE</th>
+           <th>Actions</th>
+        </tr>
+        {data.map((item,index)=>{
+          return (
+          <tr className="table-active">
+            <td key={index}>{item.id}</td>
+            <td key={index}>{item.name}</td>
+            <td key={index}>{item.age}</td>
+            <td>
+              <button  className="btn " onClick={()=> toEdit(item.id)}>Edit</button>&nbsp;&nbsp;&nbsp;
+              <button className="btn" onClick={()=>toDelete(item.id)}>Delete</button>
+             
+            </td>
+           
+          </tr>
+          )
+        })}
+      </table>
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,19 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export let  Employees = [
+    {
+        id:uuidv4(),
+        name:"Abhishek",
+        age : "22",
+    },
+    {
+        id:uuidv4(),
+        name:"Manu",
+        age : "23",
+    },
+    {
+        id:uuidv4(),
+        name:"Ashi",
+        age : "20",
+    },
+];
